86bde01fa4ce29576195bdf411911e4dd9ebb59d
ollie <lieshoff@gmx.de>
2023-01-09 19:32:07 +0100

    Changes after code generators extension.

---
+++ service/src/main/java/de/ollie/kroisos/ws/core/service/exception/JWTNotValidException.java
@@ -0,0 +1,12 @@
+package de.ollie.kroisos.ws.core.service.exception;
+
+import lombok.Generated;
+
+/**
+ * An exception for not valid JWT's.
+ *
+ * GENERATED CODE !!! DO NOT CHANGE !!!
+ */
+@Generated
+public class JWTNotValidException extends RuntimeException {
+}
